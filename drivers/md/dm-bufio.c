1973fb0f586adbd96a20e9f003d9dd48b5a82026
Mikulas Patocka <mpatocka@redhat.com>
2016-11-23 16:52:01 -0500

    BACKPORT: dm bufio: don't take the lock in dm_bufio_shrink_count
    dm_bufio_shrink_count() is called from do_shrink_slab to find out how many
    freeable objects are there. The reported value doesn't have to be precise,
    so we don't need to take the dm-bufio lock.
    
    Change-Id: I89c608454ce53f6545a03e38c8ede1b134e8e154
    Suggested-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    (cherry picked from commit d12067f428c037b4575aaeb2be00847fc214c24a)
    Signed-off-by: Minchan Kim <minchan@google.com>

--- drivers/md/dm-bufio.c
+++ drivers/md/dm-bufio.c
@@ -1563,11 +1563,13 @@ dm_bufio_shrink_scan(struct shrinker *shrink, struct shrink_control *sc)
 static unsigned long
 dm_bufio_shrink_count(struct shrinker *shrink, struct shrink_control *sc)
 {
-	struct dm_bufio_client *c = container_of(shrink, struct dm_bufio_client, shrinker);
-	unsigned long count = ACCESS_ONCE(c->n_buffers[LIST_CLEAN]) +
-			      ACCESS_ONCE(c->n_buffers[LIST_DIRTY]);
-	unsigned long retain_target = get_retain_buffers(c);
+	struct dm_bufio_client *c;
+	unsigned long count;
+	unsigned long retain_target;
 
+	c = container_of(shrink, struct dm_bufio_client, shrinker);
+	count = ACCESS_ONCE(c->n_buffers[LIST_CLEAN]) + ACCESS_ONCE(c->n_buffers[LIST_DIRTY]);
+	retain_target = get_retain_buffers(c);
 	return (count < retain_target) ? 0 : (count - retain_target);
 }
 
